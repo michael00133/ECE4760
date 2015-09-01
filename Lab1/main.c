6f35e1dd31244cf07315039ea035a2bd183295c5
Michael Nguyen <michaelnguyen@localhost.localdomain>
2015-09-02 04:36:35 +0900

    Added Libraries and such

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,1 +1,19 @@
+/*
+ * File:	main.c
+ * Author:	Michael Nguyen
+ * 		Tri Hoang
+ * 		Elias Wang
+ * Target PIC:	PIC32MX250F128B
+ */
+
+// graphics libraries
+#include "config.h"
+#include "tft_master.h"
+#include "tft_gfx.h"
+
+// Threading Library
+// config.h sets 40 MHz
+#define SYS_FREQ 40000000
+#include "pt_cornell_TFT.h"
+
 /*Code goes here */
