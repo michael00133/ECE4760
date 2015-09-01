1b3c39e7b2b2677c5ce0f05a288be327e7962b05
Michael Nguyen <michaelnguyen@localhost.localdomain>
2015-09-02 06:40:33 +0900

    Set up Blink

--- Lab1/cap_read.c
+++ Lab1/cap_read.c
@@ -16,4 +16,38 @@
 #define SYS_FREQ 40000000
 #include "pt_cornell_TFT.h"
 
-/*Code goes here */
+static struct pt pt_blink;
+
+//======================= Blink ========================= //
+// Blinks a circle on the screen at a rate of 1 blink per second
+static PT_THREAD (protothread_blink(struct pt *pt))
+{
+    PT_BEGIN(pt);
+    while(1) {
+    }
+    PT_END(pt);
+}
+
+//===================== Main ======================= //
+void main(void) {
+    SYSTEMConfigPerformance(PBCLK);
+
+    PT_setup();
+    INTEnableSystemMultiVectoredInt();
+
+    // initialize the threads
+    PT_INIT(&pt_blink);
+    
+    // initialize the display
+    tft_init_hw();
+    tft_begin();
+    tft_fillScreen(ILI9340_BLACK);
+
+    tft_setRotation(0); //240x320 vertical display
+
+    //round-robin scheduler for threads
+    while(1) {
+	PT_SCHEDULE(protothread_blink(&pt_blink));
+    }
+    
+} //end of main
