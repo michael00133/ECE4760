2221dca29485962ea33843fb46e3128d9a00e81f
Michael Nguyen <michaelnguyen@localhost.localdomain>
2015-09-04 18:27:28 +0900

    Updated All Lab 1 files

--- Lab1/nbproject/Package-default.bash
+++ Lab1/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Adafruit_TFT.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Adafruit_TFT.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=adafruittft.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Lab1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=Lab1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=lab1.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/adafruittft.x/bin
+makeDirectory ${TMPDIR}/lab1.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/adafruittft.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/lab1.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/adafruittft.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/lab1.x.tar *
 checkReturnCode
 
 # Cleanup
