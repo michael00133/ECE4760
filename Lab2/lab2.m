889a653de56178eda38358d3db4e278efef6bf28
eli <ewang921@gmail.com>
2015-09-15 14:09:54 -0400

    part 1

--- Lab2/lab2.m
+++ Lab2/lab2.m
@@ -1,27 +1,25 @@
-
+clear
 close all
-L = 500;             % Length of signal
+L = 10;             % Length of signal
 
 hold all
-for i=1:0.5:4
-Fs = 1000*i;            % Sampling frequency
+
+for i=6:10
+Fs = 1500*i;            % Sampling frequency
 T = 1/Fs;             % Sampling period
 t=(0:L-1)*T;
-S=sin(2*pi*1477*t);
-<<<<<<< HEAD
-Noise=0.5*rand(size(t));
+[St,S]=stairs(t,sin(2*pi*1477*t));
+
+Noise=0.5*rand(size(St));
 
 
-%plot(t,S)
-
-=======
-figure
-plot(t,S);
->>>>>>> 5bed4401f28f46fc902ce479daf7b1b478527b62
+L=length(St);
+% 
+% figure
+% plot(St,S);
 X=S+Noise;
 Y=fft(S);
 YN=fft(Noise);
-%plot(t,S)
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
@@ -29,17 +27,18 @@ P4 = abs(YN/L);
 P3 = P4(1:L/2+1);
 P3(2:end-1) = 2*P3(2:end-1);
 f = Fs*(0:(L/2))/L;
+figure(1)
 plot(f,20*log10(P1))
-end
 figure
-plot(f,20*log10(P3))
-title('Single-Sided Amplitude Spectrum of X(t)')
-xlabel('f (Hz)')
-ylabel('|P1(f)|')
-
-<<<<<<< HEAD
+plot(St,S);
+legendinfo{i}=num2str(i);
+end
+figure(1)
+legend(legendinfo)
+% figure
+% plot(f,20*log10(P3))
+% title('Single-Sided Amplitude Spectrum of X(t)')
+% xlabel('f (Hz)')
+% ylabel('|P1(f)|')
 
 %SNR=max(20*log10(P1))+mean(20*log10(P3))
-=======
-SNR=max(20*log10(P1))+mean(20*log10(P3))
->>>>>>> 5bed4401f28f46fc902ce479daf7b1b478527b62
