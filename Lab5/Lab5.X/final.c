538600eb7d617453daa5842534f3ec1d9fdc5be7
eli <ewang921@gmail.com>
2015-12-10 19:05:37 -0500

    clean code

--- Lab5/Lab5.X/final.c
+++ Lab5/Lab5.X/final.c
@@ -15,11 +15,7 @@
 #include <math.h>
 #include <stdint.h>
 
-// MDDFS (SD) Card libraries
-#include <stdio.h>
-#include "FSIO.h"
-#include "plib.h"
-#include <GenericTypeDefs.h>
+
 
 // Threading Library
 // config.h sets SYSCLK 40 MHz
@@ -27,49 +23,14 @@
 #include "pt_cornell_TFT.h"
 #define DAC_config_chan_A 0b0011000000000000
 
-// === the fixed point macros ========================================
-typedef signed int fix16 ;
-#define multfix16(a,b) ((fix16)(((( signed long long)(a))*(( signed long long)(b)))>>16)) //multiply two fixed 16:16
-#define float2fix16(a) ((fix16)((a)*65536.0)) // 2^16
-#define fix2float16(a) ((float)(a)/65536.0)
-#define fix2int16(a)    ((int)((a)>>16))
-#define int2fix16(a)    ((fix16)((a)<<16))
-#define divfix16(a,b) ((fix16)((((signed long long)(a)<<16)/(b)))) 
-#define sqrtfix16(a) (float2fix16(sqrt(fix2float16(a)))) 
-#define absfix16(a) abs(a)
 
-//===== Wav File Read
-#define allGood             0
-#define incorrectHeaderSize 1
-#define riff                2
-#define wav                 3
-#define fmt                 4
-#define notPCM              5
-#define stackSize           128     // cyclic buffer/stack
-#define AC_ZERO             2048    // since Dac resolution is 12-bits
-#define PB_CLK              SYS_FREQ
-
-volatile UINT16 LSTACK[stackSize];
-volatile UINT16 RSTACK[stackSize];
-volatile UINT32 BOS;
-volatile UINT32 TOS;
-
-volatile UINT32 msCounter = 0;
-volatile UINT32 randomvar = 0;
-volatile UINT32 CSlength = 0;
-volatile UINT32 j = 0;
-volatile UINT32 TIC=0, TOC=0;
-
-volatile UINT32 bufferCounter = 0;
-volatile UINT32 intCounter = 0;
 
 
 #define order 3 //order of nlms filter
 #define mu 7*pow(10,-16)       //stepsize
-UINT8 receiveBuffer[100];
-char txtBuffer[100];
 
-static struct pt pt_refresh,pt_adc;
+
+static struct pt pt_refresh;
 
 volatile unsigned int DAC_data ;// output value
 volatile SpiChannel spiChn = SPI_CHANNEL2 ;	// the SPI channel to use
@@ -80,7 +41,7 @@ char buffer[60];
 //ADC value
  int adc_9;
 int timeElapsed =1;
- int input=0;//flag for which input ADC is reading 0 for noise, 1 for signal+noise
+ 
  //primary in pin 24(muxA) ref in pin 7(muxB)
  int ref[order];
  int primary;
@@ -105,15 +66,9 @@ void update_eq(int* a, int b);
 void update_fb(int* a, int b);
 
 int  innerproduct(int* a, int* b);
-
 float  innerproductf(int* a, float* b);
 void update(int* array, int new);
-void setupAudioPWM(void);
-void getFilename(char * buffer);
-void getParameters(UINT8 * bitsPerSample, UINT8 * numberOfChannels,
-                        UINT32 * dataSize, UINT32 * sampleRate,
-                        UINT32 * blockAlign);
-UINT8 getWavHeader(FSFILE * pointer);
+
 
 //==================== Calculate ===================== //
 static PT_THREAD (protothread_refresh (struct pt *pt))
@@ -129,14 +84,9 @@ static PT_THREAD (protothread_refresh (struct pt *pt))
         tft_fillRoundRect(0,10, 320, 14, 1, ILI9340_BLACK);// x,y,w,h,radius,color
         tft_setCursor(0, 10);
         tft_setTextColor(ILI9340_WHITE); tft_setTextSize(2);
-        sprintf(buffer,"%02d:%02d  FPS:%d  Score:%d");
+        sprintf(buffer,"%02d:%02d",minutes,seconds);
         tft_writeString(buffer);
-        // draw catch bins
-        tft_fillRoundRect(120,35, 2, 5, 1, ILI9340_WHITE);// x,y,w,h,radius,color
-        tft_fillRoundRect(120,235, 2, 5, 1, ILI9340_WHITE);// x,y,w,h,radius,color
-        tft_fillRoundRect(200,35, 2, 5, 1, ILI9340_WHITE);// x,y,w,h,radius,color
-        tft_fillRoundRect(200,235, 2, 5, 1, ILI9340_WHITE);// x,y,w,h,radius,color
-
+        
         PT_YIELD_TIME_msec(1000);
         timeElapsed++ ;
         
@@ -145,34 +95,9 @@ static PT_THREAD (protothread_refresh (struct pt *pt))
   PT_END(pt);
 }
 
-// === ADC Thread =============================================
-// 
-
-static PT_THREAD (protothread_adc(struct pt *pt))
-{
-    PT_BEGIN(pt);
- 
-             
-    while(1) {
-        // yield time 1 second
-        PT_YIELD_TIME_msec(2);
-        
-       
-        // NEVER exit while
-      } // END WHILE(1)
-  PT_END(pt);
-} // animation thread
 
 void __ISR(_TIMER_3_VECTOR, ipl3) Timer3Handler(void){
     mT3ClearIntFlag();
-    /*
-    if (input==0) { //noise from an5
-        update(ref,ReadADC10(0));   // read the result of channel 9 conversion from the idle buffer;
-    }
-    else {
-        primary=ReadADC10(0);   // read the result of channel 9 conversion from the idle buffer
-    }
-    */
     primary=((ReadADC10(0)));
     int temp=(ReadADC10(1));
     update(ref,temp);
@@ -182,18 +107,18 @@ void __ISR(_TIMER_3_VECTOR, ipl3) Timer3Handler(void){
 
 void __ISR(_TIMER_4_VECTOR, ipl2) Timer4Handler(void){
     mT4ClearIntFlag();
-    //NLMS filter 
+    //LMS filter 
    desired=(int)(primary-(innerproductf(ref,weights)));
-   //desired=primary-ref[0]*weights[0]-ref[1]*weights[1]-ref[2]*weights[2]-ref[3]*weights[3]-ref[order-1]*weights[order-1];
+   
    update_eq(desired_buf,desired);
     for(i=0;i<order;i++) {
         
        weights[i]=weights[i]+(float)(mu*ref[i]*desired);
-        //weights[i]=0;
+        
     }
-   //weights[order-1]=1;
-    //DAC_data=song-desired;
+	//equalizer
    int temp=innerproduct_eq(fb_tot,desired_buf)/fixed_point;
+   //check range to prevent clipping
    if (temp>0 && temp <4096) {
     DAC_data=temp;
    }
@@ -204,27 +129,13 @@ void __ISR(_TIMER_4_VECTOR, ipl2) Timer4Handler(void){
      while (TxBufFullSPI2());
      // write to spi2 
      WriteSPI2(DAC_config_chan_A | DAC_data); //data output pin 14
-     //WriteSPI2(LSTACK[TOS]); 
-     //WriteSPI2(RSTACK[TOS]);  
-    
-   // if (++TOS == stackSize)
-     //   TOS = 0;
     // test for done
     while (SPI2STATbits.SPIBUSY); // wait for end of transaction
      // CS high
      mPORTBSetBits(BIT_4); // end transaction
 }
 
-/* ADC ISR */
-  /* Specify ADC Interrupt Routine, Priority Level = 6 */
-/*
- void __ISR(_ADC_VECTOR, ipl6) AdcHandler(void)
- {
-      
-           // clear the interrupt flag
-    mAD1ClearIntFlag();
- }
-*/
+
 //===================== Main ======================= //
 void main(void) {
     //SYSTEMConfigPerformance(PBCLK);
@@ -251,130 +162,7 @@ void main(void) {
     PPSOutput(2, RPB5, SDO2);			// MOSI for DAC
     mPORTBSetPinsDigitalOut(BIT_4);		// CS for DAC
     mPORTBSetBits(BIT_4);              // initialize CS as high
-    /*
-    
-    
-    //play music stuff
-    FSFILE * pointer;
-    
-    SearchRec rec;
-    UINT8  attributes = ATTR_MASK;   // file can have any attributes
 
-    // audio stuff (WAV file)
-    UINT8  bitsPerSample;
-    UINT32 sampleRate;
-    UINT8  numberOfChannels;
-    UINT32 dataSize;
-    UINT8  blockAlign;
-
-    UINT8  audioStream[stackSize*4];
-    UINT16 audioByte;
-    UINT16 lc;
-    UINT16 retBytes;
-    UINT16 unsign_audio;
-    
-    // clear cyclic buffer
-    BOS = 0;
-    TOS = 0;
-    for (j=0; j<stackSize; j++) {
-        LSTACK[j] = 0;
-        RSTACK[j] = 0;
-    }
-    
-    while (!MDD_MediaDetect());
-    while (!FSInit());
-   
-    pointer = FSfopen("song.wav","r");
-    if (pointer != NULL) {
-
-        if (getWavHeader(pointer) == allGood) {
-            
-            getParameters(&bitsPerSample, &numberOfChannels, &dataSize,
-                            &sampleRate, &blockAlign);
-            
-            //config hardware using sample rate
-            PR2 = (PB_CLK/sampleRate) - 1;
-            mT2IntEnable(1);
-
-            bufferCounter = 0;
-            if (bitsPerSample < 8)
-                bitsPerSample = 8;
-            else if (bitsPerSample < 16)
-                bitsPerSample = 16;
-
-            TIC = msCounter;
-
-            while (bufferCounter < dataSize) {
-
-                retBytes = FSfread(audioStream, 1,
-                                stackSize*blockAlign, pointer);
-
-                for (lc = 0; lc < retBytes; lc += blockAlign) {
-                    if (bitsPerSample == 16) {
-                        audioByte = (audioStream[lc+1] << 4) |
-                            (audioStream[lc] >> 4);
-                        if (audioByte & 0x0800) {
-                            unsign_audio = ~(audioByte - 1);
-                            audioByte = AC_ZERO - unsign_audio;
-                        }
-                        else {
-                            audioByte = AC_ZERO + audioByte;
-                        }
-                        LSTACK[BOS] = 0x3000 | audioByte;
-
-                        if (numberOfChannels == 2) {
-                            audioByte = (audioStream[lc+3] << 4) |
-                                (audioStream[lc+2] >> 4);
-                            if (audioByte & 0x0800) {
-                                unsign_audio = ~(audioByte - 1);
-                                audioByte = AC_ZERO - unsign_audio;
-                            }
-                            else {
-                                audioByte = AC_ZERO + audioByte;
-                            }
-                        }
-                        RSTACK[BOS] = 0xB000 | audioByte;
-                    }
-                    else {
-                        audioByte = audioStream[lc] << 4;
-                        LSTACK[BOS] = 0x3000 | audioByte;
-                        if (numberOfChannels == 2) {
-                            audioByte = audioStream[lc+1] << 4;
-                        }
-
-                        RSTACK[BOS] = 0xB000 | audioByte;
-                    }
-
-                    if (++BOS == stackSize) BOS = 0;
-
-                    if (bitsPerSample == 16) {
-                        while (BOS == (TOS-2));
-                    }
-                    else {
-                        while (BOS == (TOS-2)) {
-// for some reason, not putting a delay here makes the 8-bit part not work (?)
-                            UINT32 temp = msCounter;
-                            while ((msCounter - temp) < 2);
-                        }
-                    }
-
-                }   // for (lc ... )...
-
-                bufferCounter += retBytes;
-                T2CONSET = 0x8000;
-            }   // while (bufferCounter < dataSize) ...
-
-            FSfclose(pointer);
-        } // if (pointer != NULL), ie if read audio file correctly
-    }
-
-    T2CON = 0;
-    TMR2 = 0;
-    mT2IntEnable(0);
-    mT2ClearIntFlag();
-    
-    TOC = msCounter - TIC;
-    */
     // the ADC ///////////////////////////////////////
     // configure and enable the ADC
 	CloseADC10();	// ensure the ADC is off before setting the configuration
@@ -424,7 +212,6 @@ void main(void) {
     }
     // initialize the threads
     PT_INIT(&pt_refresh);
-    PT_INIT(&pt_adc);
     // initialize the display
     tft_init_hw();
     tft_begin();
@@ -438,107 +225,10 @@ void main(void) {
     while(1) {
         
         PT_SCHEDULE(protothread_refresh(&pt_refresh));
-        PT_SCHEDULE(protothread_adc(&pt_adc));
+     
     }
     
 } //main
-/*
-// Timer2, write value from audio file to the DAC
-void __ISR(_TIMER_2_VECTOR, ipl2) T2int (void) {
-    // writeDac(LSTACK[TOS]);  Write to the DAC LSTACK[TOS]
-    // writeDac(RSTACK[TOS]);  Write to the DAC RSTACK[TOS]
-    
-    if (++TOS == stackSize)
-        TOS = 0;
-
-    mT2ClearIntFlag();
-}
-*/
-void getFilename(char * buffer) {
-#define maxChars        20
-#define _BACKSPACE_     8
-
-    char c = 32;    // 32 = space
-    INT8 bufferCounter = 0;
-
-    while (DataRdyUART2() == 0);
-    c = getcUART2();
-
-    while ((c!= 10) & (c!= 13)) {
-
-        putcUART2(c);
-        while (DataRdyUART2() == 0);
-
-        *(buffer + bufferCounter) = c;
-
-        if (c == _BACKSPACE_) {
-            if (bufferCounter > 1) {
-                bufferCounter-=2;
-            }
-            else {
-                bufferCounter = 0;
-            }
-        }
-
-        if (++bufferCounter == maxChars)
-            break;
-
-        c = getcUART2();
-    }
-
-    printf("\n\r");
-
-    *(buffer + bufferCounter) = '\0';   // terminate string
-}
-
-UINT8 getWavHeader(FSFILE * pointer) {
-
-    if (FSfread(receiveBuffer, 1, 44, pointer) != 44) {
-        return incorrectHeaderSize;
-    }
-
-    if ( (receiveBuffer[0] != 'R') |
-            (receiveBuffer[1] != 'I') |
-            (receiveBuffer[2] != 'F') |
-            (receiveBuffer[3] != 'F') ) {
-        return riff;
-    }
-
-    if ( (receiveBuffer[8] != 'W') |
-            (receiveBuffer[9] != 'A') |
-            (receiveBuffer[10] != 'V') |
-            (receiveBuffer[11] != 'E') ) {
-        return wav;
-    }
-
-    if ( (receiveBuffer[12] != 'f') |
-            (receiveBuffer[13] != 'm') |
-            (receiveBuffer[14] != 't') ) {
-        return fmt;
-    }
-
-    if (receiveBuffer[20] != 1) {
-        return notPCM;
-    }
-
-    return allGood;         // no errors
-
-}
-
-void getParameters(UINT8 * bitsPerSample, UINT8 * numberOfChannels,
-                        UINT32 * dataSize, UINT32 * sampleRate,
-                        UINT32 * blockAlign) {
-    *bitsPerSample = receiveBuffer[34];
-    *numberOfChannels = receiveBuffer[22];
-    *sampleRate = (receiveBuffer[25] << 8) | receiveBuffer[24];
-
-    *dataSize = (receiveBuffer[43] << 24) |
-        (receiveBuffer[42] << 16) |
-        (receiveBuffer[41] << 8) |
-        receiveBuffer[40];
-    
-    *blockAlign = receiveBuffer[32];
-}
 
 // add new value to array and shift
 
@@ -562,7 +252,7 @@ void update_fb(int* array, int new) {
     }
     array[equal_order-1]=new;
 }
-
+//calculate inner products
 int innerproduct(int* a, int* b) {
     int sum=0;
     for (i=0;i<order;i++) {
